8d55d4e5fa9283595c545060ca8a0bf6792f62e2
phil <devnull@localhost>
2008-05-17 18:25:13 +0000

    Updated the win32-msvc2005 spec file. The manifest file for .exe files is now installed.

--- lib/siputils.py
+++ lib/siputils.py
@@ -1734,7 +1734,7 @@ class ProgramMakefile(Makefile):
             mfile.write("\t  $(OFILES) $(LIBS)\n")
             mfile.write("<<\n")
 
-            if "embed_manifest_dll" in self.optional_list("CONFIG"):
+            if "embed_manifest_exe" in self.optional_list("CONFIG"):
                 mfile.write("\tmt -nologo -manifest $(TARGET).manifest -outputresource:$(TARGET);1\n")
         elif self.generator == "BMAKE":
             mfile.write("\t$(LINK) @&&|\n")
@@ -1762,6 +1762,9 @@ class ProgramMakefile(Makefile):
 
         mfile.write("\ninstall: $(TARGET)\n")
         self.install_file(mfile, "$(TARGET)", self._install_dir)
+
+        if "embed_manifest_exe" in self.optional_list("CONFIG"):
+            self.install_file(mfile, "$(TARGET).manifest", self._install_dir)
 
     def generate_target_clean(self, mfile):
         """Generate the clean target.
