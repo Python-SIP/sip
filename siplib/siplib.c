199a51dce2d03e2ee1a8751a28e90289a308f65f
phil <devnull@localhost>
2008-10-23 17:17:25 +0000

    Fixed a silly typo in the updated converting of constrained enums.

--- siplib/siplib.c
+++ siplib/siplib.c
@@ -3195,6 +3195,8 @@ static int parsePass1(sipWrapper **selfp, int *selfargp, int *argsParsedp,
 
                         if (!PyObject_TypeCheck(arg, et))
                             valid = PARSE_TYPE;
+
+                        break;
                     }
 
                 default:
