f9eb79d29a2db78bc78fc699f753542b142cb5b9
phil <devnull@localhost>
2009-03-05 09:14:32 +0000

    Taught the build system about the new dependency of QtXmlPatterns on QtNetwork.

--- lib/siputils.py
+++ lib/siputils.py
@@ -557,7 +557,7 @@ class Makefile:
                     "QtTest":           ("QtCore", "QtGui"),
                     "QtWebKit":         ("QtCore", "QtGui", "QtNetwork"),
                     "QtXml":            ("QtCore", ),
-                    "QtXmlPatterns":    ("QtCore", ),
+                    "QtXmlPatterns":    ("QtCore", "QtNetwork"),
                     "phonon":           ("QtCore", "QtGui"),
                     "QtDesigner":       ("QtCore", "QtGui"),
                     "QAxContainer":     ("QtCore", "QtGui")
