61fe63b874b326574da53ef0a3f63409c71c7c73
Phil Thompson <phil@riverbankcomputing.com>
2013-07-11 17:52:16 +0100

    Backed out changeset bd5b9927361b The problem is real (but currently not triggered), but the fix breaks PyQt.

--- sipgen/transform.c
+++ sipgen/transform.c
@@ -2424,9 +2424,6 @@ static int sameVirtualHandler(virtHandlerDef *vhd1,virtHandlerDef *vhd2)
 {
     int a;
 
-    if (vhd1->virtcode != vhd2->virtcode)
-        return FALSE;
-
     if (isTransferVH(vhd1) != isTransferVH(vhd2))
         return FALSE;
 
