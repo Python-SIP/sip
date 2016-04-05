33911b7edd40c4107d5b20b49445d204a2488d90
Phil Thompson <phil@riverbankcomputing.com>
2016-04-05 13:40:17 +0100

    Added the rbtools product plugin.

---
+++ rbproduct.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2016, Riverbank Computing Limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+import os
+import sys
+
+from rbtools import PkgInfo, Product, TestableProduct, WheelProduct
+
+
+class SipProduct(Product, TestableProduct, WheelProduct):
+    """ The encapsulation of the sip product. """
+
+    # The name of the product.
+    name = 'sip'
+
+    # The PKG-INFO meta-data.
+    _PKG_INFO = PkgInfo(
+        description="The sip extension module required by sip generated bindings",
+        provides=['sip'])
+
+    def build(self, platform):
+        """ Build the product in the current directory using the given
+        platform.
+        """
+
+        platform.run_script('configure.py')
+        platform.run_make()
+
+    def installation(self, name):
+        """ Convert a file or directory name relative to the installation
+        directory of the product to one that is relative to site-packages.
+        """
+
+        return name
+
+    def get_imports(self):
+        """ Get the sequence of modules to test that they can be imported. """
+
+        return ('sip', )
+
+    def get_pkginfo(self):
+        """ Get the PKG-INFO meta-data. """
+
+        return self._PKG_INFO
+
+    def add_wheel_contents(self, wheel, platform):
+        """ Add the product's files to a wheel. """
+
+        wheel.add_contents(
+                (os.path.join('siplib', platform.impl_from_module('sip')), ))
