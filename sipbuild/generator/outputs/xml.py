e8ffb353d7609491878d29e0820964169ddb8e1b
Phil Thompson <phil@riverbankcomputing.com>
2022-10-23 10:06:17 +0100

    The XML is now generated as an etree root node rather than an XML file.

--- sipbuild/generator/outputs/xml.py
+++ sipbuild/generator/outputs/xml.py
@@ -21,6 +21,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 
+from xml.etree.ElementTree import Element, SubElement
+
 from ..formatters import (ArgumentFormatter, ClassFormatter, EnumFormatter,
         format_scoped_py_name, SignatureFormatter, ValueListFormatter,
         VariableFormatter)
@@ -32,32 +34,36 @@ from ..type_hints import TypeHintManager
 
 
 # The schema version number.
-_XML_VERSION_NR = 0
+_XML_VERSION_NR = '0'
 
 
-def output_xml(spec, module, xml_filename):
-    """ Output an XML export file. """
+def output_xml(spec, module_name):
+    """ Return the root Module element of the XML for a module. """
 
     # Note that we don't yet handle mapped types, templates or exceptions.
 
-    with open(xml_filename, 'w') as xf:
-        xf.write('<?xml version="1.0"?>\n')
-        xf.write(f'<Module version="{_XML_VERSION_NR}" name="{module.py_name}">\n')
+    for module in spec.modules:
+        if module.py_name == module_name:
+            break
+    else:
+        return None
 
-        for klass in spec.classes:
-            if klass.iface_file.module is module and not klass.external:
-                _xml_class(xf, spec, module, klass)
+    root = Element('Module', version=_XML_VERSION_NR, name=module.py_name)
 
-        for klass in module.proxies:
-            _xml_class(xf, spec, module, klass)
+    for klass in spec.classes:
+        if klass.iface_file.module is module and not klass.external:
+            _xml_class(root, spec, module, klass)
 
-        _xml_enums(xf, spec, module)
-        _xml_variables(xf, spec, module)
+    for klass in module.proxies:
+        _xml_class(root, spec, module, klass)
 
-        for member in module.global_functions:
-            _xml_function(xf, spec, module, member, module.overloads)
+    _xml_enums(root, spec, module)
+    _xml_variables(root, spec, module)
 
-        xf.write('</Module>\n')
+    for member in module.global_functions:
+        _xml_function(root, spec, module, member, module.overloads)
+
+    return root
 
 
 def _xml_realname(scope, member=None):
@@ -78,247 +84,158 @@ def _xml_realname(scope, member=None):
         if member is not None:
             fq_cpp_name += '::' + member
 
-    return f' realname="{fq_cpp_name}"'
+    return fq_cpp_name
 
 
-def _xml_class(xf, spec, module, klass):
+def _xml_class(parent, spec, module, klass):
     """ Output the XML for a class. """
 
-    indent = 1
-
     if klass.is_opaque:
-        s = ' ' * indent
+        return SubElement(parent, 'OpaqueClass',
+                name=ClassFormatter(spec, klass).fq_py_name)
 
-        s += '<OpaqueClass name="'
-        s += ClassFormatter(spec, klass).fq_py_name
-        s += '"/>\n'
-
-        xf.write(s)
-
-        return
-
-    if not klass.is_hidden_namespace:
-        s = ' ' * indent
-
-        s += '<Class name="'
-        s += ClassFormatter(spec, klass).fq_py_name
-        s += '"'
-
-        s += _xml_realname(klass.iface_file.fq_cpp_name)
+    if klass.is_hidden_namespace:
+        parent_klass = parent
+    else:
+        attrib = {}
 
         if klass.pickle_code is not None:
-            s += ' pickle="1"'
+            attrib['pickle'] = '1'
 
         if klass.convert_to_type_code is not None:
-            s += ' convert="1"'
+            attrib['convert'] = '1'
 
         if klass.convert_from_type_code is not None:
-            s += ' convertfrom="1"'
+            attrib['convertfrom'] = '1'
 
         if klass.real_class is not None:
-            s += f' extends="{klass.real_class.iface_file.module.py_name}"'
+            attrib['extends'] = klass.real_class.iface_file.module.py_name
 
         if klass.pyqt_flags_enums is not None:
-            s += ' flagsenums="' + ' '.join(klass.pyqt_flags_enums) + '"'
+            attrib['flagsenums'] = ' '.join(klass.pyqt_flags_enums)
 
         if len(klass.superclasses) != 0:
-            super_refs = ' '.join(
+            attrib['inherits'] = ' '.join(
                     [ClassFormatter(spec, s).rest_ref
                             for s in klass.superclasses])
 
-            s += f' inherits="{super_refs}"'
-
-        s += '>\n'
-
-        xf.write(s)
+        parent_klass = SubElement(parent, 'Class', attrib,
+                name=ClassFormatter(spec, klass).fq_py_name,
+                realname=_xml_realname(klass.iface_file.fq_cpp_name))
 
     for ctor in klass.ctors:
         if ctor.access_specifier is not AccessSpecifier.PRIVATE:
-            _xml_ctor(xf, spec, module, klass, ctor, indent + 1)
+            _xml_ctor(parent_klass, spec, module, klass, ctor)
 
-    _xml_enums(xf, spec, module, klass, indent + 1)
-    _xml_variables(xf, spec, module, klass, indent + 1)
+    _xml_enums(parent_klass, spec, module, klass)
+    _xml_variables(parent_klass, spec, module, klass)
 
     for member in klass.members:
-        _xml_function(xf, spec, module,  member, klass.overloads, klass,
-                indent + 1)
-
-    if not klass.is_hidden_namespace:
-        s = ' ' * indent
-        s += '</Class>\n'
-        xf.write(s)
+        _xml_function(parent_klass, spec, module,  member, klass.overloads,
+                klass)
 
 
-def _xml_enums(xf, spec, module, scope=None, indent=1):
+def _xml_enums(parent, spec, module, scope=None):
     """ Output the XML for all the enums in a scope. """
 
     for enum in spec.enums:
         if enum.module is module and enum.scope is scope:
             if enum.py_name is None:
                 for member in enum.members:
-                    s = ' ' * indent
-
-                    s += '<Member name="'
-                    s += format_scoped_py_name(enum.scope, member.py_name.name)
-                    s += '"'
-
-                    s += _xml_realname(enum.scope, member.cpp_name)
-
-                    s += ' const="1" typename="int"/>\n'
-
-                    xf.write(s)
+                    SubElement(parent, 'Member',
+                            name=format_scoped_py_name(enum.scope,
+                                    member.py_name.name),
+                            realname=_xml_realname(enum.scope,
+                                    member.cpp_name),
+                            const='1', typename='int')
             else:
-                s = ' ' * indent
-
-                s += '<Enum name="'
-                s += format_scoped_py_name(enum.scope, enum.py_name.name)
-                s += '"'
-
-                s += _xml_realname(enum.fq_cpp_name)
-
-                s += '>\n'
-
-                xf.write(s)
+                enum_el = SubElement(parent, 'Enum',
+                        name=format_scoped_py_name(enum.scope,
+                                enum.py_name.name),
+                        realname=_xml_realname(enum.fq_cpp_name))
 
                 for member in enum.members:
-                    s = ' ' * (indent + 1)
+                    name = format_scoped_py_name(enum.scope, enum.py_name.name) + '.' + member.py_name.name
 
-                    s += '<EnumMember name="'
-                    s += format_scoped_py_name(enum.scope, enum.py_name.name)
-                    s += '.' + member.py_name.name + '"'
-
-                    s += _xml_realname(enum.fq_cpp_name, member.cpp_name)
-
-                    s += '/>\n'
-
-                    xf.write(s)
-
-                s = ' ' * indent
-
-                s += '</Enum>\n'
-
-                xf.write(s)
+                    SubElement(enum_el, 'EnumMember', name=name,
+                            realname=_xml_realname(enum.fq_cpp_name,
+                                    member.cpp_name))
 
 
-def _xml_variables(xf, spec, module, scope=None, indent=1):
+def _xml_variables(parent, spec, module, scope=None):
     """ Output the XML for all the variables in a scope. """
 
     for variable in spec.variables:
         if variable.module is module and variable.scope is scope:
             formatter = VariableFormatter(spec, variable)
 
-            s = ' ' * indent
-
-            s += f'<Member name="{formatter.fq_py_name}"'
-
-            s += _xml_realname(variable.fq_cpp_name)
+            attrib = {}
 
             if variable.type.is_const or scope is None:
-                s += ' const="1"'
+                attrib['const'] = '1'
 
             if variable.is_static:
-                s += ' static="1"'
+                attrib['static'] = '1'
 
-            s += _xml_type(spec, module, variable.type) + '/>\n'
+            SubElement(parent, 'Member', attrib, name=formatter.fq_py_name,
+                    realname=_xml_realname(variable.fq_cpp_name),
+                    typename=_xml_typename(spec, module, variable.type))
 
-            xf.write(s)
 
-
-def _xml_ctor(xf, spec, module, scope, ctor, indent):
+def _xml_ctor(parent, spec, module, scope, ctor):
     """ Output the XML for a ctor. """
 
-    s = ' ' * indent
-
-    s += '<Function name="'
-    s += format_scoped_py_name(scope, '__init__')
-    s += '"'
-
-    s += _xml_realname(scope, '__init__')
+    attrib = {}
 
     if _has_cpp_signature(ctor.cpp_signature):
-        s += ' cppsig="'
-        s += _xml_cpp_signature(spec, ctor.cpp_signature)
-        s += '"'
+        attrib['cppsig'] = _xml_cpp_signature(spec, ctor.cpp_signature)
 
-    # Handle the trivial case.
-    if len(ctor.py_signature.args) == 0:
-        s += '/>\n'
-        xf.write(s)
-    else:
-        s += '>\n'
-        xf.write(s)
+    function_el = SubElement(parent, 'Function', attrib,
+            name=format_scoped_py_name(scope, '__init__'),
+            realname=_xml_realname(scope, '__init__'))
 
-        for arg in ctor.py_signature.args:
-            if arg.is_in:
-                _xml_argument(xf, spec, module, arg, ctor.kw_args, indent + 1)
+    for arg in ctor.py_signature.args:
+        if arg.is_in:
+            _xml_argument(function_el, spec, module, arg, ctor.kw_args)
 
-            if arg.is_out:
-                _xml_argument(xf, spec, moduke, arg, ctor.kw_args, indent + 1,
-                        out=True)
-
-        s = ' ' * indent
-        s += '</Function>\n'
-        xf.write(s)
+        if arg.is_out:
+            _xml_argument(function_el, spec, moduke, arg, ctor.kw_args,
+                    out=True)
 
 
-def _xml_function(xf, spec, module, member, overloads, scope=None, indent=1):
+def _xml_function(parent, spec, module, member, overloads, scope=None):
     """ Output the XML for a function. """
 
     for overload in overloads:
-        if overload.common is not member:
-            continue
+        if overload.common is member and overload.access_specifier is not AccessSpecifier.PRIVATE:
+            if overload.pyqt_method_specifier is PyQtMethodSpecifier.SIGNAL:
+                attrib = {}
 
-        if overload.access_specifier is AccessSpecifier.PRIVATE:
-            continue
+                if _has_cpp_signature(overload.cpp_signature):
+                    attrib['cppsig'] = _xml_cpp_signature(spec,
+                            overload.cpp_signature)
 
-        if overload.pyqt_method_specifier is PyQtMethodSpecifier.SIGNAL:
-            s = ' ' * indent
-
-            s += '<Signal name="'
-            s += format_scoped_py_name(scope, member.py_name.name)
-            s += '"'
-
-            s += _xml_realname(scope, overload.cpp_name)
-
-            if _has_cpp_signature(overload.cpp_signature):
-                s += ' cppsig="'
-                s += _xml_cpp_signature(spec, overload.cpp_signature)
-                s += '"'
-
-            # Handle the trivial case.
-            if len(overload.py_signature.args) == 0:
-                s += '/>\n'
-                xf.write(s)
-            else:
-                s += '>\n'
-                xf.write(s)
+                signal_el = SubElement(parent, 'Signal', attrib,
+                        name=format_scoped_py_name(scope, member.py_name.name),
+                        realname=_xml_realname(scope, overload.cpp_name))
 
                 for arg in overload.py_signature.args:
-                    _xml_argument(xf, spec, module, arg, overload.kw_args,
-                            indent + 1)
+                    _xml_argument(signal_el, spec, module, arg,
+                            overload.kw_args)
+            else:
+                extends = None
+                is_static = (scope is None or scope.iface_file.type is IfaceFileType.NAMESPACE or overload.is_static)
 
-                s = ' ' * indent
-                s += '</Signal>\n'
-                xf.write(s)
-        else:
-            extends = None
-            is_static = (scope is None or scope.iface_file.type is IfaceFileType.NAMESPACE or overload.is_static)
+                if scope is None and member.py_slot is not None and overload.py_signature.args[0].type is ArgumentType.CLASS:
+                    extends = overload.py_signature.args[0].definition
+                    is_static = False
 
-            if scope is None and member.py_slot is not None and overload.py_signature.args[0].type is ArgumentType.CLASS:
-                extends = overload.py_signature.args[0].definition
-                is_static = False
-
-            _xml_overload(xf, spec, module, scope, overload, extends,
-                    is_static, indent)
+                _xml_overload(parent, spec, module, scope, overload, extends,
+                        is_static)
 
 
-def _xml_overload(xf, spec, module, scope, overload, extends, is_static,
-        indent):
+def _xml_overload(parent, spec, module, scope, overload, extends, is_static):
     """ Output the XML for an overload. """
-
-    s = ' ' * indent
-
-    s += '<Function name="'
 
     if overload.is_reflected:
         name = _reflected_slot(overload.common.py_slot)
@@ -330,35 +247,31 @@ def _xml_overload(xf, spec, module, scope, overload, extends, is_static,
         cpp_name = overload.cpp_name
     else:
         cpp_name = name
-        
-    s += format_scoped_py_name(scope, name)
 
-    s += '"'
-
-    s += _xml_realname(scope, cpp_name)
+    attrib = {}
 
     if _has_cpp_signature(overload.cpp_signature):
-        s += ' cppsig="'
-        s += _xml_cpp_signature(spec, overload.cpp_signature,
+        attrib['cppsig'] = _xml_cpp_signature(spec, overload.cpp_signature,
                 is_const=overload.is_const)
-        s += '"'
 
     if overload.is_abstract:
-        s += ' abstract="1"'
+        attrib['abstract'] = '1'
 
     if is_static:
-        s += ' static="1"'
+        attrib['static'] = '1'
 
     if overload.pyqt_method_specifier is PyQtMethodSpecifier.SLOT:
-        s += ' slot="1"'
+        attrib['slot'] = '1'
 
     if overload.is_virtual:
-        s += ' virtual="1"'
+        attrib['virtual'] = '1'
 
     if extends is not None:
-        s += ' extends="'
-        s += ClassFormatter(spec, extends).fq_py_name
-        s += '"'
+        attrib['extends'] = ClassFormatter(spec, extends).fq_py_name
+
+    function_el = SubElement(parent, 'Function', attrib,
+            name=format_scoped_py_name(scope, name),
+            realname=_xml_realname(scope, cpp_name))
 
     # An empty type hint specifies a void return.
     result = overload.py_signature.result
@@ -368,18 +281,9 @@ def _xml_overload(xf, spec, module, scope, overload, extends, is_static,
     else:
         no_result = (result.type is ArgumentType.VOID and len(result.derefs) == 0)
 
-    # Handle the trivial case.
-    if no_result and len(overload.py_signature.args) == 0:
-        s += '/>\n'
-        xf.write(s)
-        return
-
-    s += '>\n'
-    xf.write(s)
-
     if not no_result:
-        _xml_argument(xf, spec, module, overload.py_signature.result,
-                KwArgs.NONE, indent + 1, out=True,
+        _xml_argument(function_el, spec, module, overload.py_signature.result,
+                KwArgs.NONE, out=True,
                 transfer_result=overload.transfer is Transfer.TRANSFER_BACK)
 
     # Ignore the first argument of non-reflected number slots and the second
@@ -395,17 +299,11 @@ def _xml_overload(xf, spec, module, scope, overload, extends, is_static,
                 continue
 
         if arg.is_in:
-            _xml_argument(xf, spec, module, arg, overload.kw_args, indent + 1)
+            _xml_argument(function_el, spec, module, arg, overload.kw_args)
 
         if arg.is_out:
-            _xml_argument(xf, spec, module, arg, overload.kw_args, indent + 1,
+            _xml_argument(function_el, spec, module, arg, overload.kw_args,
                     out=True)
-
-    s = ' ' * indent
-
-    s += '</Function>\n'
-
-    xf.write(s)
 
 
 # Argument types that imply handwritten code.
@@ -449,44 +347,39 @@ def _xml_cpp_signature(spec, signature, is_const=False):
     return f'({args}){const}'
 
 
-def _xml_argument(xf, spec, module, arg, kw_args, indent, out=False,
+def _xml_argument(parent, spec, module, arg, kw_args, out=False,
         transfer_result=False):
     """ Ouput the XML for an argument. """
 
-    if arg.array is ArrayArgument.ARRAY_SIZE:
-        return
+    if arg.array is not ArrayArgument.ARRAY_SIZE:
+        attrib = {}
 
-    s = ' ' * indent
+        if not out:
+            if arg.allow_none:
+                attrib['allownone'] = '1'
 
-    s += '<' + ('Return' if out else 'Argument')
-    s += _xml_type(spec, module, arg, kw_args=kw_args, out=out)
+            if arg.disallow_none:
+                attrib['disallownone'] = '1'
 
-    if not out:
-        if arg.allow_none:
-            s += ' allownone="1"'
+            if arg.transfer is Transfer.TRANSFER:
+                attrib['transfer'] = 'to'
+            elif arg.transfer is Transfer.TRANSFER_THIS:
+                attrib['transfer'] = 'this'
 
-        if arg.disallow_none:
-            s += ' disallownone="1"'
+        if transfer_result or arg.transfer is Transfer.TRANSFER_BACK:
+            attrib['transfer'] = 'back'
 
-        if arg.transfer is Transfer.TRANSFER:
-            s += ' transfer="to"'
-        elif arg.transfer is Transfer.TRANSFER_THIS:
-            s += ' transfer="this"'
-
-    if transfer_result or arg.transfer is Transfer.TRANSFER_BACK:
-        s += ' transfer="back"'
-
-    s += '/>\n'
-
-    xf.write(s)
+        SubElement(parent, 'Return' if out else 'Argument', attrib,
+                typename=_xml_typename(spec, module, arg, kw_args=kw_args,
+                        out=out))
 
 
-def _xml_type(spec, module, arg, kw_args=KwArgs.NONE, out=False):
+def _xml_typename(spec, module, arg, kw_args=KwArgs.NONE, out=False):
     """ Return the XML for a type. """
 
     formatter = ArgumentFormatter(spec, arg)
 
-    s = ' typename="'
+    s = ''
 
     # Handle the argument name.
     if not out and arg.name is not None:
@@ -529,8 +422,6 @@ def _xml_type(spec, module, arg, kw_args=KwArgs.NONE, out=False):
             rest_ref = formatter.py_default_value()
 
         s += rest_ref
-
-    s += '"'
 
     return s
 
