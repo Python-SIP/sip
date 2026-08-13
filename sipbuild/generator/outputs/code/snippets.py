0efd6cb8681de100314c10602cc126c945d47f49
Phil Thompson <phil@riverbankcomputing.com>
2026-08-13 20:30:44 +0100

    Support for GCC v16
    Changes to the code generated for the comparison operators to satisfy GCC
    v16's implementation of C++20.

--- sipbuild/generator/outputs/code/snippets.py
+++ sipbuild/generator/outputs/code/snippets.py
@@ -4408,11 +4408,10 @@ def _get_binary_slot_call(project, spec, scope, overload, operator,
 
         sip_cpp = '(*sipCpp)' if dereferenced else 'sipCpp'
 
-        # C++20 handles '!=' differently so we can't invoke the operator
-        # explicitly.  It's possible that there are more similar cases but this
-        # will do for now.
-        if operator == '!=':
-            slot_call += f'({sip_cpp} != '
+        # C++20 handles comparison operators differently so we can't invoke the
+        # operator explicitly.
+        if operator in _OPERATOR_COMPLEMENTS:
+            slot_call += f'({sip_cpp} {operator} '
         else:
             slot_call += f'operator{operator}({sip_cpp}, '
     else:
